d18f04274934195d2e8fa6b12c957022bfe3f289
Sebas Risco <sebas.risco@gmail.com>
2018-09-27 13:58:54 +0200

    Modify 'plant-classification-theano' script to work with new env vars

--- examples/plant-classification-theano/plant-classification-theano/script.sh
+++ examples/plant-classification-theano/plant-classification-theano/script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-OUTPUT_DIR="/tmp/output"
 echo "SCRIPT: Invoked classify_image.py. File available in $SCAR_INPUT_FILE"
 FILE_NAME=`basename $SCAR_INPUT_FILE`
-OUTPUT_FILE=$OUTPUT_DIR/$FILE_NAME
+OUTPUT_FILE=$SCAR_OUTPUT_FOLDER/$FILE_NAME
 
 python2 /opt/plant-classification-theano/classify_image.py $SCAR_INPUT_FILE -o $OUTPUT_FILE
