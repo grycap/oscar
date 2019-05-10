9655da2ca17351b130675b214da409f0ee67f683
Sebas Risco <sebas.risco@gmail.com>
2019-05-10 14:13:58 +0200

    Fix issues with user-scripts in examples add quotation marks to avoid problems with whitespaces

--- examples/video-process/yolov3-object-detection.sh
+++ examples/video-process/yolov3-object-detection.sh
@@ -9,4 +9,4 @@ OUTPUT_IMAGE="$TMP_OUTPUT_DIR/$FILENAME"
 echo "SCRIPT: Analyzing file '$INPUT_FILE_PATH', saving the result in '$RESULT' and the output image in '$OUTPUT_IMAGE'"
 
 cd /opt/darknet
-./darknet detect cfg/yolov3.cfg yolov3.weights $INPUT_FILE_PATH -out $OUTPUT_IMAGE > $RESULT
+./darknet detect cfg/yolov3.cfg yolov3.weights "$INPUT_FILE_PATH" -out "$OUTPUT_IMAGE" > $RESULT
