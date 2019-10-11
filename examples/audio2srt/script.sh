4aaac80650aaf2ced613f09426d2f34be199f1ef
Sebas Risco <sebas.risco@gmail.com>
2019-10-11 13:14:35 +0200

    Add audio2srt example

---
+++ examples/audio2srt/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "SCRIPT: Invoked audio2srt. File available in $INPUT_FILE_PATH"
+FILE_NAME=`basename "$INPUT_FILE_PATH"`
+OUTPUT_FILE="$TMP_OUTPUT_DIR/$FILE_NAME"
+echo "SCRIPT: Generating subtitles from audio file $INPUT_FILE_PATH to $OUTPUT_FILE"
+./audio2srt.py -i "$INPUT_FILE_PATH" -o "$OUTPUT_FILE" -- -lm en-us.lm.bin -hmm en-us
