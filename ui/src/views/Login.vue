f9e187690e3a8b2a1420a9b5ec57cdde2c060cff
Sebas Risco <sebas.risco@gmail.com>
2020-07-13 12:44:46 +0200

    Fix OSCAR-UI login

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -101,7 +101,7 @@ export default {
         var _this = this
           axios({
                 method: 'get',
-                url: this.api+'/system/config',
+                url: '/system/config',
                 auth: {
                     username: this.model.username,
                     password: this.model.password
