0eed7cd44601db2b010f926145d36ee4d2b14eb7
dianamariand92 <dianamariand92@gmail.com>
2020-09-07 14:15:45 +0200

    fixing login problem

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -101,7 +101,7 @@ export default {
         var _this = this
           axios({
                 method: 'get',
-                url: 'https://158.42.105.207/system/config',
+                url: '/system/config',
                 auth: {
                     username: this.model.username,
                     password: this.model.password
