a6219cf75451f666059a8d8f5bdb36dd87485850
Sebas Risco <sebas.risco@gmail.com>
2021-11-08 18:38:23 +0100

    Fixes in cowsay

--- examples/cowsay/script.sh
+++ examples/cowsay/script.sh
@@ -4,5 +4,5 @@ if [ "$INPUT_TYPE" = "json" ]
 then
     jq '.message' "$INPUT_FILE_PATH" -r | /usr/games/cowsay
 else
-    base64 -d "$INPUT_FILE_PATH" | /usr/games/cowsay
+    cat "$INPUT_FILE_PATH" | /usr/games/cowsay
 fi
