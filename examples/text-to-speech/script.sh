5db4219e1fd8ffc4302874a75ea8cecedc70dbe3
Sebas Risco <sebas.risco@gmail.com>
2022-05-13 13:46:22 +0200

    Remove comments in text-to-speech script

--- examples/text-to-speech/script.sh
+++ examples/text-to-speech/script.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 FILE_NAME=${INPUT_FILE_PATH##*/}
 OUTPUT_FILE="$TMP_OUTPUT_DIR/$FILE_NAME"
-#echo 'esto es una prueba'
+
 #echo "SCRIPT: Invoked tts.py. File available in $INPUT_FILE_PATH"
-#echo "SCRIPT: Invoked tts.py. FILE_NAME in $FILE_NAME"
-#echo "SCRIPT: Invoked tts.py. OUTPUT_FILE $TMP_OUTPUT_DIR/"
-#echo "SCRIPT: Invoked tts.py. lenguage $lenguage"
-#echo "python3 /opt/tts.py --lenguage="$lenguage" -o "$OUTPUT_FILE" "$INPUT_FILE_PATH""
 python3 /opt/tts.py --language="$language" -o "$OUTPUT_FILE" "$INPUT_FILE_PATH"
