e8126994fe310804f2269d01ee97f88ced88cfff
catttam <caterinaalarconm@gmail.com>
2022-07-12 12:57:56 +0200

    Changes in fire-detection example

--- examples/fire-detection/script.sh
+++ examples/fire-detection/script.sh
@@ -2,5 +2,9 @@
 
 echo "SCRIPT: Invoked inference_ff_oscar.py with file $INPUT_FILE_PATH"
 FILE_NAME=`basename $INPUT_FILE_PATH`
+filename_wo_extension="${FILE_NAME%.*}"
 mv $INPUT_FILE_PATH "$INPUT_FILE_PATH.jpeg"
-python3 efficient-compact-fire-detection-cnn/inference_ff_oscar.py --image "$INPUT_FILE_PATH.jpeg" --output "$TMP_OUTPUT_DIR" --model shufflenetonfire --weight efficient-compact-fire-detection-cnn/weights/shufflenet_ff.pt > "$OUTPUT_FILE.txt"
+
+python3 efficient-compact-fire-detection-cnn/inference_ff_oscar.py --image "$INPUT_FILE_PATH.jpeg" --output "$TMP_OUTPUT_DIR" --model shufflenetonfire --weight efficient-compact-fire-detection-cnn/weights/shufflenet_ff.pt
+
+zip -r $filename_wo_extension $TMP_OUTPUT_DIR
