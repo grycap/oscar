0175eb7e792e772e809d1d855c747cb7c733446f
Sebas Risco <sebas.risco@gmail.com>
2022-08-03 14:11:28 +0200

    Update script and add FDL with replicas

--- examples/fire-detection/script.sh
+++ examples/fire-detection/script.sh
@@ -7,7 +7,7 @@ cp "$INPUT_FILE_PATH" $TMP_OUTPUT_DIR"/input-"$FILE_NAME
 
 python3 efficient-compact-fire-detection-cnn/inference_ff_oscar.py --image $INPUT_FILE_PATH --output $TMP_OUTPUT_DIR --model shufflenetonfire --weight efficient-compact-fire-detection-cnn/weights/shufflenet_ff.pt
 
-zip -r "output-$filename_wo_extension.zip" $TMP_OUTPUT_DIR
+zip -r -j "output-$filename_wo_extension.zip" $TMP_OUTPUT_DIR
 mv output-$filename_wo_extension.zip $TMP_OUTPUT_DIR
 
 if $SEND_SNS ; then
