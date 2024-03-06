cd7b8eeed58c25b35b6fdbdc22b4e9df79b447f5
vicente87 <vicente.rdguezb@gmail.com>
2024-03-06 16:25:08 +0100

    annotation error1

--- pkg/handlers/status.go
+++ pkg/handlers/status.go
@@ -22,8 +22,10 @@ import (
 	    "fmt"
 	    "os"
 	    "encoding/json"
+		"context"
 	"net/http"
 	"k8s.io/client-go/kubernetes"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 //    versioned "k8s.io/metrics/pkg/client/clientset/versioned/typed/metrics/v1beta1"
 )
 
