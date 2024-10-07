363256558c5f0c2ff3ab9fd20c91f5012d19ce2c
Germán Moltó <gmolto@dsic.upv.es>
2024-10-07 19:11:54 +0200

    Revert "Included Deployment support for macOS"
    This reverts commit 09d3a4f90343368ef2a22dffa59af0d9ceb1431d.

--- deploy/kind-deploy.sh
+++ deploy/kind-deploy.sh
@@ -49,7 +49,7 @@ checkDocker(){
     else
         echo -e "$GREEN$CHECK$END_COLOR Docker installation found"
 
-        rep=$(docker info)
+        rep=$(curl -s --unix-socket /var/run/docker.sock http://ping > /dev/null)
         status=$?
 
         if [ "$status" == "7" ]; then
