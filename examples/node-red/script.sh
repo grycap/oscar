1849d5944a9db794bf5fdddcd98b2d5e121218b4
Robert Kazaryan <kazaryanrobert@gmail.com>
2025-05-29 12:24:55 +0200

    fix: update Node-RED directory paths and enhance startup script

--- examples/node-red/script.sh
+++ examples/node-red/script.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-node-red --port 1880 -D uiHost="::" -D httpRoot=$NODE_RED_BASE_URL &
+sleep 15
+mkdir -p $NODE_RED_DIRECTORY
+cp -r /root/.node-red/* "$NODE_RED_DIRECTORY"/
+
+node-red --port 1880 --userDir $NODE_RED_DIRECTORY -D uiHost="::" -D httpRoot=$NODE_RED_BASE_URL &
 
 while true; do
   sleep 1
