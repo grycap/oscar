0ca66ddeea056a7761aa00ea0199d4e28ff38e38
Robert Kazaryan <kazaryanrobert@gmail.com>
2025-05-29 14:04:23 +0200

    add: create fdl.yaml and script.sh for Node-RED authentication example

---
+++ examples/node-red/with_auth/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sleep 15
+mkdir -p $NODE_RED_DIRECTORY
+cp -r /root/.node-red/* "$NODE_RED_DIRECTORY"/
+
+NODE_RED_PWD_HASH=$(echo "$NODE_RED_PWD" | node-red admin hash-pw | cut -d' ' -f2)
+
+node-red --port 1880 --userDir $NODE_RED_DIRECTORY \
+  -D uiHost="::" \
+  -D httpRoot=$NODE_RED_BASE_URL \
+  -D adminAuth="{\"type\":\"credentials\",\"users\":[{\"username\":\"admin\",\"password\":\"$NODE_RED_PWD_HASH\",\"permissions\":\"*\"}]}" \
+  &
+
+while true; do
+  sleep 1
+done
