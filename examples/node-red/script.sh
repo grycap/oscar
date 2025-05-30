1515964613f021acc2a79b2c156bf4afbbdf3908
Robert Kazaryan <kazaryanrobert@gmail.com>
2025-05-30 10:11:01 +0200

    fix: bug that do not allow download new nodes

--- examples/node-red/script.sh
+++ examples/node-red/script.sh
@@ -2,10 +2,10 @@
 
 sleep 15
 mkdir -p $NODE_RED_DIRECTORY
-cp -r /root/.node-red/* "$NODE_RED_DIRECTORY"/
 
-node-red --port 1880 --userDir $NODE_RED_DIRECTORY -D uiHost="::" -D httpRoot=$NODE_RED_BASE_URL &
+node-red --port 1880 --userDir $NODE_RED_DIRECTORY -D uiHost="::" -D credentialSecret=false -D contextStorage.default.module=localfilesystem -D httpRoot=$NODE_RED_BASE_URL &
 
 while true; do
   sleep 1
 done
+
--- examples/node-red/with_auth/script.sh
+++ examples/node-red/with_auth/script.sh
@@ -2,13 +2,14 @@
 
 sleep 15
 mkdir -p $NODE_RED_DIRECTORY
-cp -r /root/.node-red/* "$NODE_RED_DIRECTORY"/
 
 NODE_RED_PWD_HASH=$(echo "$NODE_RED_PWD" | node-red admin hash-pw | cut -d' ' -f2)
 
 node-red --port 1880 --userDir $NODE_RED_DIRECTORY \
   -D uiHost="::" \
   -D httpRoot=$NODE_RED_BASE_URL \
+  -D credentialSecret=false \
+  -D contextStorage.default.module=localfilesystem \
   -D adminAuth="{\"type\":\"credentials\",\"users\":[{\"username\":\"admin\",\"password\":\"$NODE_RED_PWD_HASH\",\"permissions\":\"*\"}]}" \
   &
 
