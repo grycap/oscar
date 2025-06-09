942354cac539863c9c8752e916f5778e5d18fd7f
Robert Kazaryan <kazaryanrobert@gmail.com>
2025-06-09 10:12:22 +0200

    update: restructure Dockerfile to separate subflows and examples, enhance script for directory creation

--- examples/node-red/with_auth/script.sh
+++ examples/node-red/with_auth/script.sh
@@ -5,7 +5,9 @@ mkdir -p $NODE_RED_DIRECTORY
 mkdir -p $NODE_RED_DIRECTORY/lib
 mkdir -p $NODE_RED_DIRECTORY/lib/flows
 mkdir -p $NODE_RED_DIRECTORY/lib/flows/oscar-subflows
+mkdir -p $NODE_RED_DIRECTORY/lib/flows/oscar-examples
 cp /oscar-subflows/* $NODE_RED_DIRECTORY/lib/flows/oscar-subflows
+cp /oscar-examples/* $NODE_RED_DIRECTORY/lib/flows/oscar-examples
 
 NODE_RED_PWD_HASH=$(echo "$PASSWORD" | node-red admin hash-pw | cut -d' ' -f2)
 
