6eaff6fb95d398c481364bdf622584ac67f5a94b
juanttito <juanttito@gmail.com>
2025-09-09 01:58:05 +0200

    Add vLLM + LLama example

---
+++ examples/vllm-llama/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 -m vllm.entrypoints.openai.api_server \
+  --root-path $OPENAI_BASE_URL \
+  --model unsloth/Llama-3.2-1B-Instruct \
+  --max_model_len $MAX_MODEL_LEN \
+  --enforce-eager \
+  --gpu-memory-utilization $GPU_MEMORY_UTILIZATION
