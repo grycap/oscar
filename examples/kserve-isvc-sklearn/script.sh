e182832aaca516d0391198cdc8b381059ceb3e14
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-03-23 14:42:31 +0100

    update kserve-isvc-sklearn example

--- examples/kserve-isvc-sklearn/script.sh
+++ examples/kserve-isvc-sklearn/script.sh
@@ -2,5 +2,5 @@
 
 echo "Invoked isvc sklearn. File available in $INPUT_FILE_PATH"
 cat "$INPUT_FILE_PATH"
-#curl -v -H "Content-Type: application/json" http://sklearn-iris.kserve-test.svc.cluster.local/v1/models/sklearn-iris:predict -d @/tmp/tmpzc1b5o4i/iris-input.json
-curl -v -H "Content-Type: application/json" http://sklearn-iris.kserve-test.svc.cluster.local/v1/models/sklearn-iris:predict -d @/$INPUT_FILE_PATH
+
+curl -v -H "Content-Type: application/json" http://$KSERVE_HOST/v1/models/kserve-isvc-sklearn:predict -d @/$INPUT_FILE_PATH
