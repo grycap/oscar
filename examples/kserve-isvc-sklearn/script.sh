8fcbf4453927ddbd90932e679865974ac0d89680
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-03-24 15:45:29 +0100

    fix: update script to save prediction output

--- examples/kserve-isvc-sklearn/script.sh
+++ examples/kserve-isvc-sklearn/script.sh
@@ -2,5 +2,8 @@
 
 echo "Invoked isvc sklearn. File available in $INPUT_FILE_PATH"
 cat "$INPUT_FILE_PATH"
+PAYLOAD_NAME=$(basename "$INPUT_FILE_PATH")
+OUTPUT_FILE="$TMP_OUTPUT_DIR/output_$PAYLOAD_NAME"
 
-curl -v -H "Content-Type: application/json" http://$KSERVE_HOST/v1/models/kserve-isvc-sklearn:predict -d @/$INPUT_FILE_PATH
+curl -v -H "Content-Type: application/json" http://$KSERVE_HOST/v1/models/kserve-isvc-sklearn:predict -d @/$INPUT_FILE_PATH > $OUTPUT_FILE
+cat $OUTPUT_FILE
