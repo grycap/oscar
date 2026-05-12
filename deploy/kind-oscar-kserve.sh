4645cbd81ab3018bb806deba1b08dab254c8442b
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-05-12 10:32:32 +0200

    fix: change helm install to upgrade for KServe resources in deployment script

--- deploy/kind-oscar-kserve.sh
+++ deploy/kind-oscar-kserve.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
 if kubectl get namespace cert-manager >/dev/null 2>&1 && \
   kubectl get deployment -n cert-manager cert-manager cert-manager-cainjector cert-manager-webhook >/dev/null 2>&1; then
   echo "cert-manager already installed, skipping install"
@@ -10,13 +13,13 @@ kubectl wait --namespace cert-manager \
   cert-manager cert-manager-cainjector cert-manager-webhook \
   --timeout=300s
 
-helm install kserve-crd oci://ghcr.io/kserve/charts/kserve-crd \
+helm upgrade --install kserve-crd oci://ghcr.io/kserve/charts/kserve-crd \
   --version v0.18.0 \
   --namespace kserve \
   --create-namespace \
   --wait
 
-helm install kserve-resources oci://ghcr.io/kserve/charts/kserve-resources \
+helm upgrade --install kserve-resources oci://ghcr.io/kserve/charts/kserve-resources \
   --version v0.18.0 \
   --namespace kserve \
   --set kserve.controller.deploymentMode=Standard \
@@ -27,20 +30,20 @@ helm install kserve-resources oci://ghcr.io/kserve/charts/kserve-resources \
   --set kserve.controller.gateway.ingressGateway.createGateway=false \
   --wait
 
-helm install kserve-llmisvc-crd oci://ghcr.io/kserve/charts/kserve-llmisvc-crd \
+helm upgrade --install kserve-llmisvc-crd oci://ghcr.io/kserve/charts/kserve-llmisvc-crd \
   --version v0.18.0 \
   --namespace kserve \
   --create-namespace \
   --wait
 
-helm install kserve-llmisvc-resources oci://ghcr.io/kserve/charts/kserve-llmisvc-resources \
+helm upgrade --install kserve-llmisvc-resources oci://ghcr.io/kserve/charts/kserve-llmisvc-resources \
   --version v0.18.0 \
   --create-namespace \
   --namespace kserve \
   --set kserve.createSharedResources=false \
   --wait
 
-helm install kserve-runtime-configs oci://ghcr.io/kserve/charts/kserve-runtime-configs \
+helm upgrade --install kserve-runtime-configs oci://ghcr.io/kserve/charts/kserve-runtime-configs \
   --version v0.18.0 \
   --namespace kserve \
   --set kserve.llmisvcConfigs.enabled=true \
