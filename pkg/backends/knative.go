a462d728234b7158f6528657ecd7ffeb37558a0c
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-07-02 10:54:17 +0200

    fix: Delete associated secret when creating a service EnsureServiceVolume fails

--- pkg/backends/knative.go
+++ pkg/backends/knative.go
@@ -142,6 +142,12 @@ func (kn *KnativeBackend) CreateService(service types.Service) error {
 		if delErr := deleteServiceConfigMap(service.Name, namespace, kn.kubeClientset); delErr != nil {
 			log.Println(delErr.Error())
 		}
+		if utils.SecretExists(service.Name, namespace, kn.kubeClientset) {
+			secretsErr := utils.DeleteSecret(service.Name, namespace, kn.kubeClientset)
+			if secretsErr != nil {
+				log.Printf("Error deleting associated secret: %v", secretsErr)
+			}
+		}
 		return err
 	}
 
