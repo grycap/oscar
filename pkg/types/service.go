e59d736743a5bee2ff0f16f56f0ccb828def0c72
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-09-15 17:32:37 +0200

    fix: ensure CPU validation only occurs if service image is provided

--- pkg/types/service.go
+++ pkg/types/service.go
@@ -424,8 +424,10 @@ func (s Service) Validate() error {
 		return fmt.Errorf("service Image is required")
 	}
 
-	if cpu, err := resource.ParseQuantity(s.CPU); err != nil || cpu.Cmp(resource.MustParse("0.2")) < 0 {
-		return fmt.Errorf("service CPU must be greater than or equal to 0.2")
+	if strings.TrimSpace(s.Image) != "" {
+		if cpu, err := resource.ParseQuantity(s.CPU); err != nil || cpu.Cmp(resource.MustParse("0.2")) < 0 {
+			return fmt.Errorf("service CPU must be greater than or equal to 0.2")
+		}
 	}
 
 	if s.IsKserve() {
